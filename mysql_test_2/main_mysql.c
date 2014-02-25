b4e4a64f60f5162b4973315aa7cb78e80aab0785
LEO HU <superleo.peking@gmail.com>
2014-02-25 21:59:36 +0800

    mysql + c query database

---
+++ mysql_test_2/main_mysql.c
@@ -0,0 +1,9 @@
+/* Simple C program that connects to MySQL Database server*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "test_1.h"
+
+int main() {
+   show_master_tables();
+   return 0;
+}
---
+++ mysql_test_2/test_1.h
@@ -0,0 +1,3 @@
+/* Simple C program that connects to MySQL Database server*/
+
+void show_master_tables();
